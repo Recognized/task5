9650651a9b352e1b514089270338fd8e8ca4f6ec
Vlad Saifulin <vladislav.saifulin@jetbrains.com>
2019-05-06 12:46:03 +0300

    reformat

--- src/components/letter/letter.jsx
+++ src/components/letter/letter.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import { Checkbox } from '../checkbox/checkbox';
 
 import './letter.css';
--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,8 +1,10 @@
 import React, { PureComponent } from 'react';
+
 import { SidebarLogo } from '../sidebar-logo/sidebar-logo';
 import { SidebarButtonNewLetter } from '../sidebar-button-new-letter/sidebar-button-new-letter';
 import { SidebarMenu } from '../sidebar-menu/sidebar-menu';
 import { SidebarMenuItem } from '../sidebar-menu-item/sidebar-menu-item';
+
 import './sidebar.css';
 import './sidebar__logo-container.css';
 
